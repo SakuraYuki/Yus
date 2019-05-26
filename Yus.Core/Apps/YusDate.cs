b6ad844a39921b7d432aa43961ebc8dffa7d3b70
Yuki <yuki1015@live.com>
2019-05-26 20:19:22 +0800

    1.修复时间不是24时制 2.升级版本到v1.0.1.3.

--- Yus.Core/Apps/YusDate.cs
+++ Yus.Core/Apps/YusDate.cs
@@ -17,19 +17,19 @@ namespace Yus.Apps
         /// <summary>日期格式（中文），12时30分50秒</summary>
         public const string TimeCN = "hh时mm分ss秒";
         /// <summary>时间日期格式（中文），2008年01月01日 12时30分50秒</summary>
-        public const string DateTimeCN = "yyyy年MM月dd日 hh:mm:ss";
+        public const string DateTimeCN = "yyyy年MM月dd日 HH:mm:ss";
         /// <summary>时间日期格式，包含毫秒，2008-01-01 12:30:50 356</summary>
-        public const string LongTime = "yyyy年MM月dd日 hh:mm:ss ffff";
+        public const string LongTime = "yyyy年MM月dd日 HH:mm:ss ffff";
         /// <summary>时间日期格式，包含毫秒（中文），2008年01月01日 12时30分50秒 356毫秒</summary>
-        public const string LongTimeCN = "yyyy年MM月dd日 hh:mm:ss ffff";
+        public const string LongTimeCN = "yyyy年MM月dd日 HH:mm:ss ffff";
         /// <summary>日期格式，下划线连接，2008_01_01</summary>
         public const string DateUnderline = "yyyy_MM_dd";
         /// <summary>日期格式，下划线连接，12_30_50</summary>
-        public const string TimeUnderline = "hh_mm_ss";
+        public const string TimeUnderline = "HH_mm_ss";
         /// <summary>时间日期格式，下划线连接，2008_01_01_12_30_50</summary>
-        public const string DateTimeUnderline = "yyyy_MM_dd_hh_mm_ss";
+        public const string DateTimeUnderline = "yyyy_MM_dd_HH_mm_ss";
         /// <summary>时间日期格式，包含毫秒，下划线连接，2008_01_01_12_30_50_356</summary>
-        public const string LongTimeUnderline = "yyyy_MM_dd_hh_mm_ss_ffff";
+        public const string LongTimeUnderline = "yyyy_MM_dd_HH_mm_ss_ffff";
         /// <summary>日期格式，斜杠连接，2008/01/01</summary>
         public const string DateSlash = "yyyy/MM/dd";
 
