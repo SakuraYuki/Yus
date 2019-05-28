0b1522cd2166b425df668982960ac519969b8c4e
Yuki <yuki1015@live.com>
2019-05-28 21:34:13 +0800

    1.修复时间模块错误 2.更新版本到v1.0.1.4.

--- Yus.Core/Apps/YusDate.cs
+++ Yus.Core/Apps/YusDate.cs
@@ -11,17 +11,17 @@ namespace Yus.Apps
         /// <summary>时间格式，2008-01-01</summary>
         public const string Date = "yyyy-MM-dd";
         /// <summary>日期格式，12:30:50</summary>
-        public const string Time = "hh:mm:ss";
+        public const string Time = "HH:mm:ss";
         /// <summary>时间格式（中文），2008年01月01日</summary>
         public const string DateCN = "yyyy年MM月dd日";
         /// <summary>日期格式（中文），12时30分50秒</summary>
-        public const string TimeCN = "hh时mm分ss秒";
+        public const string TimeCN = "HH时mm分ss秒";
         /// <summary>时间日期格式（中文），2008年01月01日 12时30分50秒</summary>
-        public const string DateTimeCN = "yyyy年MM月dd日 HH:mm:ss";
+        public const string DateTimeCN = "yyyy年MM月dd日 HH时mm分ss秒";
         /// <summary>时间日期格式，包含毫秒，2008-01-01 12:30:50 356</summary>
-        public const string LongTime = "yyyy年MM月dd日 HH:mm:ss ffff";
+        public const string LongTime = "yyyy-MM-dd HH:mm:ss fff";
         /// <summary>时间日期格式，包含毫秒（中文），2008年01月01日 12时30分50秒 356毫秒</summary>
-        public const string LongTimeCN = "yyyy年MM月dd日 HH:mm:ss ffff";
+        public const string LongTimeCN = "yyyy年MM月dd日 HH时mm分ss秒 fff毫秒";
         /// <summary>日期格式，下划线连接，2008_01_01</summary>
         public const string DateUnderline = "yyyy_MM_dd";
         /// <summary>日期格式，下划线连接，12_30_50</summary>
@@ -29,7 +29,7 @@ namespace Yus.Apps
         /// <summary>时间日期格式，下划线连接，2008_01_01_12_30_50</summary>
         public const string DateTimeUnderline = "yyyy_MM_dd_HH_mm_ss";
         /// <summary>时间日期格式，包含毫秒，下划线连接，2008_01_01_12_30_50_356</summary>
-        public const string LongTimeUnderline = "yyyy_MM_dd_HH_mm_ss_ffff";
+        public const string LongTimeUnderline = "yyyy_MM_dd_HH_mm_ss_fff";
         /// <summary>日期格式，斜杠连接，2008/01/01</summary>
         public const string DateSlash = "yyyy/MM/dd";
 
--- Yus.Core/Properties/AssemblyInfo.cs
+++ Yus.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Yus.Core")]
-[assembly: AssemblyDescription("Yus框架-核心模块")]
+[assembly: AssemblyDescription("Yus框架-核心模块，丰富的扩展方法，易于使用的工具类库")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("YusTech")]
 [assembly: AssemblyProduct("Yus技术团队出品")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
+[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyFileVersion("1.0.1.4")]
